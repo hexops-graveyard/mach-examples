d64104f245b066ee6f9c04607758fed5b053e697
BlackGoku36 <urjasvisuthar@gmail.com>
2023-03-22 22:33:34 +0530

    core: procedural-primitives: use MultiArrayList for cylinder example

--- core/procedural-primitives/procedural-primitives.zig
+++ core/procedural-primitives/procedural-primitives.zig
@@ -184,24 +184,21 @@ pub fn createCubePrimitive(allocator: std.mem.Allocator, size: f32) !Primitive {
     return Primitive{ .vertex_data = vertex_data, .vertex_count = vertex_count, .index_data = index_data, .index_count = index_count, .type = .quad };
 }
 
+const VertexDataMAL = std.MultiArrayList(VertexData);
+
 pub fn createCylinderPrimitive(allocator: std.mem.Allocator, radius: f32, height: f32, num_sides: u32) !Primitive {
     const alloc_amt_vert: u32 = (num_sides * 12) / 3;
     const alloc_amt_idx: u32 = num_sides * 12;
 
-    var vertex_data = try std.ArrayList(VertexData).initCapacity(allocator, alloc_amt_vert);
+    var vertex_data = VertexDataMAL{};
+    try vertex_data.ensureTotalCapacity(allocator, alloc_amt_vert);
+    defer vertex_data.deinit(allocator);
+
+    var out_vertex_data = try std.ArrayList(VertexData).initCapacity(allocator, alloc_amt_vert);
     var index_data = try std.ArrayList(u32).initCapacity(allocator, alloc_amt_idx);
 
-    // (50 * 12)/3 = 200 (allocate for 50 side)
-    var temp_normal: [200 * 3]f32 = undefined;
-    var temp_vertices: [200 * 3]f32 = undefined;
-
-    temp_vertices[0] = 0.0;
-    temp_vertices[1] = (height / 2.0);
-    temp_vertices[2] = 0.0;
-
-    temp_vertices[3] = 0.0;
-    temp_vertices[4] = -((height / 2.0));
-    temp_vertices[5] = 0.0;
+    vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{0.0, (height/2.0), 0.0}, .normal = undefined});
+    vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{0.0, -(height/2.0), 0.0}, .normal = undefined});
 
     const angle = 2.0 * PI / @intToFloat(f32, num_sides);
 
@@ -211,13 +208,9 @@ pub fn createCylinderPrimitive(allocator: std.mem.Allocator, radius: f32, height
         const x: f32 = radius * zmath.sin(angle * float_i);
         const y: f32 = radius * zmath.cos(angle * float_i);
 
-        temp_vertices[i * 6] = x;
-        temp_vertices[i * 6 + 1] = (height / 2.0);
-        temp_vertices[i * 6 + 2] = y;
+        vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{x, (height/2.0), y}, .normal = undefined});
+        vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{x, -(height/2.0), y}, .normal = undefined});
 
-        temp_vertices[i * 6 + 3] = x;
-        temp_vertices[i * 6 + 4] = -(height / 2.0);
-        temp_vertices[i * 6 + 5] = y;
     }
 
     var group1: u32 = 1;
@@ -242,30 +235,30 @@ pub fn createCylinderPrimitive(allocator: std.mem.Allocator, radius: f32, height
             var indexB: u32 = index_data.items[i + 1];
             var indexC: u32 = index_data.items[i + 2];
 
-            var vert1: F32x4 = F32x4{ temp_vertices[indexA * 3], temp_vertices[indexA * 3 + 1], temp_vertices[indexA * 3 + 2], 1.0 };
-            var vert2: F32x4 = F32x4{ temp_vertices[indexB * 3], temp_vertices[indexB * 3 + 1], temp_vertices[indexB * 3 + 2], 1.0 };
-            var vert3: F32x4 = F32x4{ temp_vertices[indexC * 3], temp_vertices[indexC * 3 + 1], temp_vertices[indexC * 3 + 2], 1.0 };
+            var vert1: F32x4 = F32x4{ vertex_data.get(indexA).position[0],vertex_data.get(indexA).position[1],vertex_data.get(indexA).position[2], 1.0 };
+            var vert2: F32x4 = F32x4{ vertex_data.get(indexB).position[0],vertex_data.get(indexB).position[1],vertex_data.get(indexB).position[2], 1.0 };
+            var vert3: F32x4 = F32x4{ vertex_data.get(indexC).position[0],vertex_data.get(indexC).position[1],vertex_data.get(indexC).position[2], 1.0 };
 
             var edgeAB: F32x4 = vert2 - vert1;
             var edgeAC: F32x4 = vert3 - vert1;
 
             var cross = zmath.cross3(edgeAB, edgeAC);
 
-            temp_normal[indexA * 3] += cross[0];
-            temp_normal[indexA * 3 + 1] += cross[1];
-            temp_normal[indexA * 3 + 2] += cross[2];
-            temp_normal[indexB * 3] += cross[0];
-            temp_normal[indexB * 3 + 1] += cross[1];
-            temp_normal[indexB * 3 + 2] += cross[2];
-            temp_normal[indexC * 3] += cross[0];
-            temp_normal[indexC * 3 + 1] += cross[1];
-            temp_normal[indexC * 3 + 2] += cross[2];
+            vertex_data.items(.normal)[indexA][0] += cross[0];
+            vertex_data.items(.normal)[indexA][1] += cross[1];
+            vertex_data.items(.normal)[indexA][2] += cross[2];
+            vertex_data.items(.normal)[indexB][0] += cross[0];
+            vertex_data.items(.normal)[indexB][1] += cross[1];
+            vertex_data.items(.normal)[indexB][2] += cross[2];
+            vertex_data.items(.normal)[indexC][0] += cross[0];
+            vertex_data.items(.normal)[indexC][1] += cross[1];
+            vertex_data.items(.normal)[indexC][2] += cross[2];
         }
     }
 
-    for (0..alloc_amt_vert) |i| {
-        vertex_data.appendAssumeCapacity(VertexData{ .position = F32x3{ temp_vertices[i * 3], temp_vertices[i * 3 + 1], temp_vertices[i * 3 + 2] }, .normal = F32x3{ temp_normal[i * 3], temp_normal[i * 3 + 1], temp_normal[i * 3 + 2] } });
+    for(vertex_data.items(.position), vertex_data.items(.normal)) |pos, nor|{
+        out_vertex_data.appendAssumeCapacity(VertexData{.position = pos, .normal = nor});
     }
 
-    return Primitive{ .vertex_data = vertex_data, .vertex_count = alloc_amt_vert, .index_data = index_data, .index_count = alloc_amt_idx, .type = .cylinder };
+    return Primitive{ .vertex_data = out_vertex_data, .vertex_count = alloc_amt_vert, .index_data = index_data, .index_count = alloc_amt_idx, .type = .cylinder };
 }
