4322115fd753b3c35f593813681e64c468ed6309
Urjasvi Suthar <36535717+BlackGoku36@users.noreply.github.com>
2023-03-24 22:55:06 +0530

    Add cylinder to Procedural Primitive examples (#41)
    * core: procedural-primitives: add cylinder
    
    * core: procedural-primitives: fmt
    
    * core: procedural-primitives: hoist some code out of loop
    
    * core: procedural-primitives: rename normal -> temp_normal in createCylinderProcedural(..)
    
    * core: procedural-primitives: fix cylinder verts/indexes allocation amount
    
    * core: procedural-primitives: remove un-necessary std.mem.set()'s
    
    * fix generation
    
    * core: procedural-primitives: clean up
    
    * core: procedural-primitives: use MultiArrayList for cylinder example

--- core/procedural-primitives/procedural-primitives.zig
+++ core/procedural-primitives/procedural-primitives.zig
@@ -183,3 +183,82 @@ pub fn createCubePrimitive(allocator: std.mem.Allocator, size: f32) !Primitive {
 
     return Primitive{ .vertex_data = vertex_data, .vertex_count = vertex_count, .index_data = index_data, .index_count = index_count, .type = .quad };
 }
+
+const VertexDataMAL = std.MultiArrayList(VertexData);
+
+pub fn createCylinderPrimitive(allocator: std.mem.Allocator, radius: f32, height: f32, num_sides: u32) !Primitive {
+    const alloc_amt_vert: u32 = (num_sides * 12) / 3;
+    const alloc_amt_idx: u32 = num_sides * 12;
+
+    var vertex_data = VertexDataMAL{};
+    try vertex_data.ensureTotalCapacity(allocator, alloc_amt_vert);
+    defer vertex_data.deinit(allocator);
+
+    var out_vertex_data = try std.ArrayList(VertexData).initCapacity(allocator, alloc_amt_vert);
+    var index_data = try std.ArrayList(u32).initCapacity(allocator, alloc_amt_idx);
+
+    vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{0.0, (height/2.0), 0.0}, .normal = undefined});
+    vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{0.0, -(height/2.0), 0.0}, .normal = undefined});
+
+    const angle = 2.0 * PI / @intToFloat(f32, num_sides);
+
+    for (1..num_sides + 1) |i| {
+        var float_i = @intToFloat(f32, i);
+
+        const x: f32 = radius * zmath.sin(angle * float_i);
+        const y: f32 = radius * zmath.cos(angle * float_i);
+
+        vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{x, (height/2.0), y}, .normal = undefined});
+        vertex_data.appendAssumeCapacity(VertexData{.position = F32x3{x, -(height/2.0), y}, .normal = undefined});
+
+    }
+
+    var group1: u32 = 1;
+    var group2: u32 = 3;
+
+    for (0..num_sides) |_| {
+        if (group2 >= num_sides * 2) group2 = 1;
+        index_data.appendSliceAssumeCapacity(&[_]u32{
+            0,          group1 + 1, group2 + 1,
+            group1 + 1, group1 + 2, group2 + 1,
+            group1 + 2, group2 + 2, group2 + 1,
+            group2 + 2, group1 + 2, 1,
+        });
+        group1 += 2;
+        group2 += 2;
+    }
+
+    {
+        var i: u32 = 0;
+        while (i < alloc_amt_idx) : (i += 3) {
+            var indexA: u32 = index_data.items[i];
+            var indexB: u32 = index_data.items[i + 1];
+            var indexC: u32 = index_data.items[i + 2];
+
+            var vert1: F32x4 = F32x4{ vertex_data.get(indexA).position[0],vertex_data.get(indexA).position[1],vertex_data.get(indexA).position[2], 1.0 };
+            var vert2: F32x4 = F32x4{ vertex_data.get(indexB).position[0],vertex_data.get(indexB).position[1],vertex_data.get(indexB).position[2], 1.0 };
+            var vert3: F32x4 = F32x4{ vertex_data.get(indexC).position[0],vertex_data.get(indexC).position[1],vertex_data.get(indexC).position[2], 1.0 };
+
+            var edgeAB: F32x4 = vert2 - vert1;
+            var edgeAC: F32x4 = vert3 - vert1;
+
+            var cross = zmath.cross3(edgeAB, edgeAC);
+
+            vertex_data.items(.normal)[indexA][0] += cross[0];
+            vertex_data.items(.normal)[indexA][1] += cross[1];
+            vertex_data.items(.normal)[indexA][2] += cross[2];
+            vertex_data.items(.normal)[indexB][0] += cross[0];
+            vertex_data.items(.normal)[indexB][1] += cross[1];
+            vertex_data.items(.normal)[indexB][2] += cross[2];
+            vertex_data.items(.normal)[indexC][0] += cross[0];
+            vertex_data.items(.normal)[indexC][1] += cross[1];
+            vertex_data.items(.normal)[indexC][2] += cross[2];
+        }
+    }
+
+    for(vertex_data.items(.position), vertex_data.items(.normal)) |pos, nor|{
+        out_vertex_data.appendAssumeCapacity(VertexData{.position = pos, .normal = nor});
+    }
+
+    return Primitive{ .vertex_data = out_vertex_data, .vertex_count = alloc_amt_vert, .index_data = index_data, .index_count = alloc_amt_idx, .type = .cylinder };
+}
