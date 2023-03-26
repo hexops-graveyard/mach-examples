c8c23748d01cea4f00bd7cdd3b11aa92fa6cd729
Stephen Gutekanst <stephen@hexops.com>
2023-03-26 11:17:41 -0700

    ecs-app: prepare to handle init via ECS modules
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- engine/ecs-app/main.zig
+++ engine/ecs-app/main.zig
@@ -22,32 +22,52 @@ const physics2d = @import("physics2d.zig");
 // so cannot be renamed here.
 // TODO: just make this a list so one cannot even think renaming is possible here
 const modules = ecs.Modules(.{
+    .game = game,
     .mach = mach.module,
     .renderer = renderer.module,
     .physics2d = physics2d.module,
 });
 
 // Our Mach app, which tells Mach where to find our modules and init entry point.
-pub const App = mach.App(modules, init);
+pub const App = mach.App(modules);
 
-pub fn init(engine: *ecs.World(modules)) !void {
-    // The Mach .core is where we set window options, etc.
-    const core = engine.get(.mach, .core);
-    core.setTitle("Hello, ECS!");
+pub const Message = ecs.Messages(.{
+    .init = void,
+});
 
-    // We can get the GPU device:
-    const device = engine.get(.mach, .device);
-    _ = device; // TODO: actually show off using the GPU device
+pub const game = ecs.Module(.{
+    .components = .{},
+    .messages = Message,
+    .update = update,
+});
 
-    // We can create entities, and set components on them. Note that components live in a module
-    // namespace, so we set the `.renderer, .location` component which is different than the
-    // `.physics2d, .location` component.
+// TODO: inject `engine: *ecs.World(modules)` without a dependency loop
+fn update(msg: Message) void {
+    switch (msg) {
+        .init => {
+            std.debug.print("game init!", .{});
+            // // The Mach .core is where we set window options, etc.
+            // const core = engine.get(.mach, .core);
+            // core.setTitle("Hello, ECS!");
 
-    // TODO: cut out the `.entities.` in this API to make it more brief
-    const player = try engine.entities.new();
-    try engine.entities.setComponent(player, .renderer, .location, .{ .x = 0, .y = 0, .z = 0 });
-    try engine.entities.setComponent(player, .physics2d, .location, .{ .x = 0, .y = 0 });
+            // // We can get the GPU device:
+            // const device = engine.get(.mach, .device);
+            // _ = device; // TODO: actually show off using the GPU device
 
-    // TODO: there could be an entities wrapper to interact with a single namespace so you don't
-    // have to pass it in as a parameter always?
+            // // We can create entities, and set components on them. Note that components live in a module
+            // // namespace, so we set the `.renderer, .location` component which is different than the
+            // // `.physics2d, .location` component.
+
+            // // TODO: cut out the `.entities.` in this API to make it more brief
+            // const player = try engine.entities.new();
+            // try engine.entities.setComponent(player, .renderer, .location, .{ .x = 0, .y = 0, .z = 0 });
+            // try engine.entities.setComponent(player, .physics2d, .location, .{ .x = 0, .y = 0 });
+
+            // // TODO: there could be an entities wrapper to interact with a single namespace so you don't
+            // // have to pass it in as a parameter always?
+        },
+    }
 }
+
+// pub fn init(engine: *ecs.World(modules)) !void {
+// }
