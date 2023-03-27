3192ed75ee0659abf79130d8ba6ee6b3a9ec8f7f
Stephen Gutekanst <stephen@hexops.com>
2023-03-26 20:46:24 -0700

    ecs-app: update to new message handler pattern
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- engine/ecs-app/Game.zig
+++ engine/ecs-app/Game.zig
@@ -14,35 +14,27 @@ const ecs = mach.ecs;
 //
 pub const name = .game;
 
-pub const Message = ecs.Messages(.{
-    .init = void,
-});
+pub fn init(adapter: anytype) !void {
+    std.debug.print("game init!", .{});
+    // The Mach .core is where we set window options, etc.
+    const core = adapter.get(.mach, .core);
+    core.setTitle("Hello, ECS!");
 
-pub fn update(adapter: anytype, msg: Message) !void {
-    switch (msg) {
-        .init => {
-            std.debug.print("game init!", .{});
-            // The Mach .core is where we set window options, etc.
-            const core = adapter.get(.mach, .core);
-            core.setTitle("Hello, ECS!");
+    // We can get the GPU device:
+    const device = adapter.get(.mach, .device);
+    _ = device; // TODO: actually show off using the GPU device
 
-            // We can get the GPU device:
-            const device = adapter.get(.mach, .device);
-            _ = device; // TODO: actually show off using the GPU device
+    // We can create entities, and set components on them. Note that components live in a module
+    // namespace, so we set the `.renderer, .location` component which is different than the
+    // `.physics2d, .location` component.
 
-            // We can create entities, and set components on them. Note that components live in a module
-            // namespace, so we set the `.renderer, .location` component which is different than the
-            // `.physics2d, .location` component.
+    // TODO: cut out the `.entities.` in this API to make it more brief
+    const player = try adapter.entities.new();
+    try adapter.entities.setComponent(player, .renderer, .location, .{ .x = 0, .y = 0, .z = 0 });
+    try adapter.entities.setComponent(player, .physics2d, .location, .{ .x = 0, .y = 0 });
 
-            // TODO: cut out the `.entities.` in this API to make it more brief
-            const player = try adapter.entities.new();
-            try adapter.entities.setComponent(player, .renderer, .location, .{ .x = 0, .y = 0, .z = 0 });
-            try adapter.entities.setComponent(player, .physics2d, .location, .{ .x = 0, .y = 0 });
+    // TODO: there could be an entities wrapper to interact with a single namespace so you don't
+    // have to pass it in as a parameter always?
 
-            // TODO: there could be an entities wrapper to interact with a single namespace so you don't
-            // have to pass it in as a parameter always?
-
-            adapter.set(.mach, .exit, true);
-        },
-    }
+    adapter.set(.mach, .exit, true);
 }
--- engine/ecs-app/physics2d.zig
+++ engine/ecs-app/physics2d.zig
@@ -4,10 +4,6 @@ const std = @import("std");
 
 pub const name = .physics2d;
 
-pub const Message = ecs.Messages(.{
-    .tick = void,
-});
-
 pub const components = .{
     .location = Vec2,
     .rotation = Vec2,
@@ -16,10 +12,7 @@ pub const components = .{
 
 pub const Vec2 = extern struct { x: f32, y: f32 };
 
-pub fn update(adapter: anytype, msg: Message) !void {
+pub fn tick(adapter: anytype) !void {
     _ = adapter;
-    switch (msg) {
-        // TODO: implement queries, ability to set components, etc.
-        .tick => std.log.debug("physics tick!", .{}),
-    }
+    std.log.debug("physics tick!", .{});
 }
