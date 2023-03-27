107226f463a1cc8c34acb49e632320123b8bfdc6
Stephen Gutekanst <stephen@hexops.com>
2023-03-26 20:24:24 -0700

    ecs-app: clarify module naming
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- engine/ecs-app/main.zig
+++ engine/ecs-app/main.zig
@@ -10,15 +10,8 @@ const Game = @import("Game.zig");
 // A Mach app is just a list of all the modules in our application. Our game itself is implemented
 // in our own module called Game.
 //
-// TODO(engine): reevaluate module names docs below
-//
-// Modules can have components, systems, state, and/or global values in them. They can also send and
-// receive messages to coordinate with each-other.
-//
-// Single-word module names (`.mach`, `.renderer`, etc.) are reserved for the application itself.
-//
-// Modules that come from libraries must be prefixed (e.g. `.bullet_physics`, `.ziglibs_box2d`)
-// similar to GitHub repositories, to avoid conflicts with one another.
+// Modules can have components, systems, state, etc. They can also send and receive messages to
+// coordinate with each-other.
 pub const App = mach.App(.{
     Game,
     mach.Module,
