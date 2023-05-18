691573fd298d86ca438892c6aac059273bfd490d
Stephen Gutekanst <stephen@hexops.com>
2023-05-17 20:39:04 -0700

    engine: sprite2d: move math and gfx2d into main mach codebase
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- engine/sprite2d/main.zig
+++ engine/sprite2d/main.zig
@@ -1,13 +1,12 @@
 // Experimental ECS app example. Not yet ready for actual use.
 const mach = @import("mach");
 
-const Sprite2D = @import("gfx2d/Sprite2D.zig");
 const Game = @import("Game.zig");
 
 // The list of modules to be used in our application. Our game itself is implemented in our own
 // module called Game.
 pub const App = mach.App(.{
     mach.Module,
-    Sprite2D,
+    mach.gfx2d.Sprite2D,
     Game,
 });
